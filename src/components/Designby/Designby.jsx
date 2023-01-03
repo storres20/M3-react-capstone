e9f9dd2e6347454c5874ed4bf8e2b4eab097dbbb
storres20 <sanchotorres777@gmail.com>
2023-01-02 23:20:19 -0500

    Add Designby component

---
+++ src/components/Designby/Designby.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Designby.scss';
+
+function Designby() {
+  return (
+    <div className="designContainer">
+      <p>
+        Designed by
+        {' '}
+        <a href="https://myport20.netlify.app/" target="_blank" rel="noreferrer"><b>ITALO</b></a>
+      </p>
+    </div>
+  );
+}
+
+export default Designby;
