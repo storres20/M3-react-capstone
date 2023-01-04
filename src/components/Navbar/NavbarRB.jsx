a4301e2f37fc33cde0bfc7853fa58f3e3440171a
storres20 <sanchotorres777@gmail.com>
2023-01-03 21:58:45 -0500

    Fix code reviewer suggestions for styles

--- src/components/Navbar/NavbarRB.jsx
+++ src/components/Navbar/NavbarRB.jsx
@@ -10,7 +10,10 @@ export default function NavbarRB() {
   return (
     <Navbar bg="dark" expand="md" sticky="top" variant="dark">
       <Container>
-        <Navbar.Brand className={styles.NavTitle}><NavLink to="/">Movies</NavLink></Navbar.Brand>
+        <Navbar.Brand className={styles.NavTitle}>
+          <NavLink to="/">&#60;</NavLink>
+          <NavLink to="/">Movies</NavLink>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className={`me-auto ${styles.NavBar}`}>
